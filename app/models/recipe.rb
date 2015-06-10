661ee54d638820c4294ad5a6126b4358507de1a0
Neniah <maria.lobillo.santos@gmail.com>
2015-06-10 12:28:36 +0200

    recipe test

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :summary, presence: true
+
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -16,4 +16,39 @@ class RecipeTest < ActiveSupport::TestCase
     assert_not @recipe.valid?
   end
 
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+
+  test "name length should not be too short" do
+    @recipe.name = "aaaa"
+    assert_not @recipe.valid?
+  end
+
+  test "summary should be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+
+  test "summary lenght should not be too long" do
+
+  end
+
+  test "summary length should not be too short" do
+
+  end
+
+  test "description must be present" do
+
+  end
+
+  test "description should not be too long" do
+
+  end
+
+  test "description should not be too short" do
+
+  end
+
 end
