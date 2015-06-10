7bc0db51f9a43af382b61229641365d8f6019c6a
Neniah <maria.lobillo.santos@gmail.com>
2015-06-10 20:54:58 +0200

    asserts

--- test/models/cocinero_test.rb
+++ test/models/cocinero_test.rb
@@ -6,12 +6,12 @@ class CocineroTest < ActiveSupport::TestCase
     @cocinero = Cocinero.new(name: "jhon", email: 'jhon@mail.com')
   end
 
-  def "cocinero should be valid" do 
-    assert @cocinero.valid
+  test "cocinero should be valid" do 
+    assert @cocinero.valid?
   end
 
-  def "cocinero should be present" do
-    @cocinero.name = ' '
+  test "cocinero should be present" do
+    @cocinero.name = " "
     assert_not @cocinero.valid?
   end
 end
