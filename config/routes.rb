d54381038b386703992d2435f4d6160e9415e996
Neniah <maria.lobillo.santos@gmail.com>
2015-06-24 20:26:23 +0200

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
-  get '/recipes', to: 'recipes#index'
-  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
-  post '/recipes', to: 'recipes#create'
-  get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
-  patch '/recipes/:id', to: 'recipes#update'
-  get 'recipes/:id', to: 'recipes#show'
-  delete '/recipes/:id', to: 'recipes#destroy'
+
+
+  root "recipes#index"
+  resources :recipes
 
 
 end
